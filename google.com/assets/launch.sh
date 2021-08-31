631b0900b409f835de4b594b23c55069db182e47
Aditya Chaterjee <speak2adi@gmail.com>
2021-08-31 11:33:16 +0530

    running kubernetes
    Signed-off-by: Aditya Chaterjee <speak2adi@gmail.com>

---
+++ google.com/assets/launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Waiting for the environment to be set up"
+
+sleep 8s
+
+echo "Setting up a Kubernetes"
+
+sleep 10s
+
+echo "Your environment has been set up with a Kubernetes running. You may now begin the tutorial"
